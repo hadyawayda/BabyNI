fa7d1fa541f1bc5d078b6f42fa8ed448fa10aef4
Hady Awayda <hady_awayda@hotmail.com>
2023-11-30 23:54:33 +0200

    changes

---
+++ babyapi/Data/Models/EntityModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BabyAPI.Data.Models
+{
+    [Keyless]
+
+    public class BabyModel
+    {
+        public int DATETIME_KEY { get; set; }
+        public DateTime TIME { get; set; }
+        public int NETWORK_SID { get; set; }
+        public string? NEALIAS { get; set; }
+        public string? NETYPE { get; set; }
+        public int RSL_INPUT_POWER { get; set; }
+        public int MAX_RX_LEVEL { get; set; }
+        public int RSL_DEVIATION { get; set; }
+
+
+        // Add Optional Data Annotations or Fluent API
+    }
+
+}

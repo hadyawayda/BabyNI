568e1137e8940641d5b340ee528c03c495821169
Hady Awayda <hady_awayda@hotmail.com>
2023-12-01 20:14:07 +0200

    Updated

--- ui/app/loading.tsx
+++ ui/app/loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
    return (
-      <div className="flex justify-center items-center fixed">
+      <div className="flex justify-center items-center fixed inset-0 text-red-800">
          <div className="flex justify-center items-center">Loading Data...</div>
       </div>
    )
